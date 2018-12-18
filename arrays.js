8795baf4f85fba37a35f48f6528109beec22acfa
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-18 22:36:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,5 +4,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element){
   var array = ['foo']
   var element = (1)
-  return array.unshift(element)
+  return array.push(element)
 }
