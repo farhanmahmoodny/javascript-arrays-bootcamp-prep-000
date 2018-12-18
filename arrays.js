1a70c371960056f7f57456dab4e5d0ea790bd94b
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-18 23:06:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,4 +9,4 @@ addElementToBeginningOfArray([1], 'foo')
 function destructivelyAddElementToBeginningOfArray(array, element){
   return  array.unshift(element)
 }
-destructivelyAddElementToBeginningOfArray([1], 'foo')
+destructivelyAddElementToBeginningOfArray(['foo'], 'foo')
