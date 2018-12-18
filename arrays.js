d774b6a927d70daa9ed8dd9993cdcfeff9dbb3e7
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-18 22:29:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 var states = ['NY', 'NJ', 'PA']
 function addElementToBeginningOfArray(array, element){
-  return states.unshift('VA')
+  return states.unshift('foo')
 }
